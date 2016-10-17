0fb3cbd2a7f3610f9beb65ea97c4e1d5a3b08670
Sheela Gowry <sheelagowry@gmail.com>
2016-10-17 18:28:48 +0300

    Add files via upload

---
+++ conf.php
@@ -0,0 +1,6 @@
+<?php
+// fill the blanks and rename the file to conf.php
+define('DB_HOST', '172.16.0.71');
+define('DB_NAME', 'ssumathi_db');
+define('DB_USER', 'ssumathi');
+define('DB_PASS', 'changeme');
